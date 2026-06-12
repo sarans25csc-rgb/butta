baee82880d7c55fcbfab402a122d9fde9ccdfc56
sarans25csc-rgb <sarans25csc@vetias.ac.in>
2026-06-12 14:46:56 +0530

    added datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+console.log("welcome to primitive datatypes")
+//numbers
+console.log("DT numbers");
+var age = 30;
+console.log(age);// output: 30
+//string
+console.log("DT string");
+var name = "jhon";
+console.log(name);// output: jhon
+//booleans
+console.group("DT booleans");
+var isstudent = true;
+console.log(isstudent);// output: true
+//null
+console.log("DT null");
+var address = null;
+console.log(address);//output: null
+//undefined
+console.log("DT undefined");
+var phone;
+console.log("phone");//output: undefined
