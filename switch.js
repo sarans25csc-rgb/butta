b664831663a63711afbacab152bd628baffc4a72
sarans25csc-rgb <sarans25csc@vetias.ac.in>
2026-06-15 10:53:56 +0530

    added switch.js

---
+++ switch.js
@@ -0,0 +1,15 @@
+var signal = "white"
+switch (signal){
+    case "green":
+        console.log("go!");
+        break;
+        case "yellow":
+            console.log("wait!");
+            break;
+         case "red":
+         console.log("stop!");
+         break;
+         
+         default:
+            console.log("Invalid light colour");
+}
